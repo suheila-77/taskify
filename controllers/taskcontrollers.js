c1b98bbb5a12af442afb43e68b3489fe647db62f
suheila-77 <zuheilaupdalla7@gmail.com>
2024-11-03 19:39:37 +0300

    handling form data

--- controllers/taskcontrollers.js
+++ controllers/taskcontrollers.js
@@ -1,7 +1,23 @@
+const IncomingForm = require("formidable/Formidable");
 const { readTasksFromFile } = require("../utils/fileHandler")
 
 exports.getTasks=(req,res)=>{
   const tasks = readTasksFromFile();
   res.writehead(200, {'content-type': 'application/json'})
   res.end(JSON.stringify(tasks))
+}
+
+
+exports.createTasks = (req,res)=>{
+    const form = new IncomingForm()
+    form.parse(req,(err, fields,files)=>{
+        if(err){
+            res.writehead(400, {'content-type': 'appliction/json'}),
+                res.end(JSON.stringify({
+                    message: "Eror parsing form"
+                }))
+                return;
+            
+        }
+    })
 }
