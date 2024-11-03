8b9cdb77f96666a34abdac8db18927c9093ea02f
suheila-77 <zuheilaupdalla7@gmail.com>
2024-11-03 19:08:20 +0300

    writing tasks to the file

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -2,3 +2,7 @@ const fs = require ("fs")
  const path = require ("path")
 
  const filepath = './data/tasks.json'
+
+ const writeTasksTofile = (tasks)=>{
+    fs.writeFileSync(filepath, JSON.stringify(tasks))
+ }
