2be291b1b583268924a10c17e5116ad8c976173d
suheila-77 <zuheilaupdalla7@gmail.com>
2024-11-04 19:35:03 +0300

    finally fixed the image part

--- controllers/taskcontrollers.js
+++ controllers/taskcontrollers.js
@@ -1,48 +1,52 @@
 const { IncomingForm } = require('formidable');
 const { readTasksFromFile, writeTasksToFile } = require("../utils/fileHandler");
+
 const { copyFileSync } = require('fs');
 const path = require('path');
 
 exports.getTasks = (req, res) => {
     const tasks = readTasksFromFile();
-    res.writeHead(200, { 'content-type': 'application/json' });
-    res.end(JSON.stringify(tasks));
-};
+    res.writeHead(200, { 'content-type': 'application/json'})
+    res.end(JSON.stringify(tasks))
+}
 
 exports.createTask = (req, res) => {
     const form = new IncomingForm();
     form.parse(req, (err, fields, files) => {
-        if (err) {
-            res.writeHead(400, { 'content-type': 'application/json' });
+        if(err) {
+            res.writeHead(400, { 'content-type': 'application/json'});
             res.end(JSON.stringify({
                 message: 'Error parsing form'
-            }));
+            }))
             return;
         }
 
-        const image = files.image ? files.image : null;
+      
 
-        const tasks = readTasksFromFile();
+        const image = files.image ? files.image[0] : ""
+
+        const tasks = readTasksFromFile()
 
         const newTask = {
             id: Date.now(),
-            title: fields.title,
-            description: fields.description || '',
-            status: fields.status || 'pending',
+            title: fields?.title,
+            description: fields?.description || '',
+            status: fields?.status || 'pending',
             image: image ? `/uploads/${image.originalFilename}` : null,
-        };
+        }
 
         tasks.push(newTask);
+
         writeTasksToFile(tasks);
 
-        if (image) {
+        if(image) {
             copyFileSync(image.filepath, path.join(__dirname, '../uploads', image.originalFilename));
         }
 
-        res.writeHead(200, { 'content-type': 'application/json' });
-        res.end(JSON.stringify(newTask));
-    });
-};
+        res.writeHead(200, { 'content-type': 'application/json'});
+        res.end(JSON.stringify(newTask))
+    })
+}
 
 exports.updateTask = (req, res) => {
     res.end(JSON.stringify({
--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,18 +1,18 @@
-const fs = require ("fs")
- const path = require ("path")
+const fs = require("fs");
+const path = require("path");
 
- const filepath = './data/tasks.json'
+const filepath = './data/tasks.json';
 
- exports.writeTasksToFile = (tasks)=>{
-    fs.writeFileSync(filepath, JSON.stringify(tasks))
- }
+exports.writeTasksToFile = (tasks) => {
+    fs.writeFileSync(filepath, JSON.stringify(tasks, null, 2));
+};
 
- exports.readTasksFromFile = ()=>{
-    if(!fs.existsSync(filepath)){
-        this.writeTasksTofile([])
+exports.readTasksFromFile = () => {
+    if (!fs.existsSync(filepath)) {
+        this.writeTasksToFile([]);  
     }
 
     const data = fs.readFileSync(filepath);
-    return JSON.parse(data)
 
- }
+    return JSON.parse(data);  
+};
